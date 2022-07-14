fd1d6d34d5c7e99136dbc40d5c53f60ce66c2c3e
Beto Betin <95883258+betinb623@users.noreply.github.com>
2022-07-14 21:43:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+exibirAsteriscos (5);
+
+function exibirAsteriscos(linhas) {
+  let padrao = '';
+    for (let linha = 1; linha <= linhas; linha++) {
+      padrao += '*';
+      console.log (padrao);
+    }
+}
